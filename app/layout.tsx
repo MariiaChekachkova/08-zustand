65874d51da35213eb7b709c9b9057b8f3ea27834
Mariia Chekachkova <chekachkovam@gmail.com>
2026-09-06 17:45:20 +0200

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,20 @@ export const metadata: Metadata = {
   title: "NoteHub",
   description:
     "A simple and efficient application for managing personal notes.",
+  openGraph: {
+    title: "NoteHub",
+    description:
+      "A simple and efficient application for managing personal notes.",
+    url: "https://notehub.com",
+    images: [
+      {
+        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
+        width: 1200,
+        height: 630,
+        alt: "NoteHub",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
